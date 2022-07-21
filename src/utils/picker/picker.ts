12d4813a44afed1f1e5cb67cde7c38b0a5720e04
Jovan Blažek <36740941+jovanblazek@users.noreply.github.com>
2022-07-21 22:46:15 +0200

    Added picker integration tests

--- src/utils/picker/picker.ts
+++ src/utils/picker/picker.ts
@@ -1,6 +1,13 @@
 import { App } from '@slack/bolt'
 import { formatMessage, sendMessage } from '../messages'
-import { getModerators } from './getModerators'
+import {
+  filterBots,
+  filterIgnoredMembers,
+  getChannelMembersIds,
+  getRandomArrayElements,
+} from './utils'
+
+const MODERATOR_COUNT = 2
 
 interface PickerOptions {
   channelId: string
@@ -12,7 +19,11 @@ export const picker = async (
   { channelId, message, ignoredMembers = [] }: PickerOptions,
   slackAppInstance: App
 ) => {
-  const moderators = await getModerators({ channelId, ignoredMembers }, slackAppInstance)
+  const channelMembers = await getChannelMembersIds(channelId, slackAppInstance)
+  const filteredChannelMembers = filterIgnoredMembers(channelMembers, ignoredMembers)
+  const humanMembers = await filterBots(filteredChannelMembers, slackAppInstance)
+  const moderators = getRandomArrayElements(humanMembers, MODERATOR_COUNT)
+
   if (!moderators || !moderators.length) {
     throw new Error('No moderators generated')
   }
