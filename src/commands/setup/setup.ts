51c6f6831d7d64633f2609b4e78039a370801db4
Jovan Blažek <36740941+jovanblazek@users.noreply.github.com>
2022-09-24 13:59:13 +0200

    Add setup modal validation

---
+++ src/commands/setup/setup.ts
@@ -0,0 +1,62 @@
+import { AllMiddlewareArgs, App, SlackViewAction, SlackViewMiddlewareArgs } from '@slack/bolt'
+import { Command } from '../../classes'
+import { CommandNames } from '../../constants'
+import { scheduleCronJob } from '../../utils/cron'
+import Log from '../../utils/logger'
+import { sendMessage } from '../../utils/messages'
+import { getSetupModal } from '../../utils/modals/modalGenerators'
+import Prisma from '../../utils/prismaClient'
+import { generateErrorMessage, getModalData, validateSetupModalInput } from './validationSchema'
+
+export const handleSetupModalSubmit = async (
+  { ack, body }: SlackViewMiddlewareArgs<SlackViewAction> & AllMiddlewareArgs,
+  slackAppInstance: App
+) => {
+  await ack()
+  const userInput = getModalData(body)
+  const validationResult = validateSetupModalInput(userInput)
+
+  if (validationResult.success) {
+    const { channelId, userId, schedule, message, ignoredMembers } = validationResult.data
+    const ignoredMembersStringified = JSON.stringify(ignoredMembers)
+
+    await Prisma.cron.upsert({
+      where: { channelId },
+      create: { channelId, schedule, message, ignoredMembers: ignoredMembersStringified },
+      update: { schedule, message, ignoredMembers: ignoredMembersStringified },
+    })
+    Log.info(`Upserted cron job for ${channelId} with schedule ${schedule}`)
+
+    scheduleCronJob(schedule, { channelId, ignoredMembers, message }, slackAppInstance)
+    await sendMessage({ channelId, userId, text: 'Aye aye sir! 🫡' }, slackAppInstance)
+    return
+  }
+  if (userInput.channelId && userInput.userId) {
+    const { channelId, userId } = userInput
+    await sendMessage(
+      {
+        channelId,
+        userId,
+        text: `⚠️ There was an error with your input:\n${generateErrorMessage(
+          validationResult.error
+        )}`,
+      },
+      slackAppInstance
+    )
+  }
+}
+
+export default new Command(
+  {
+    name: CommandNames.setup,
+    description: 'Setup a scheduled job for this channel',
+  },
+  async ({ ack, payload }, slackAppInstance) => {
+    await ack()
+
+    await slackAppInstance.client.views.open({
+      trigger_id: payload.trigger_id,
+      view: getSetupModal({ channelId: payload.channel_id }),
+    })
+  }
+)
---
+++ src/commands/setup/validationSchema.ts
@@ -0,0 +1,57 @@
+import { SlackViewAction } from '@slack/bolt'
+import { get, reduce } from 'lodash'
+import { validate } from 'node-cron'
+import { typeToFlattenedError, z } from 'zod'
+import { BlockIds } from '../../constants'
+import { parseJson } from '../../utils/formatters'
+
+export type UserInputType = z.infer<typeof SetupModalValidationSchema>
+
+const SetupModalValidationSchema = z.object({
+  channelId: z.string().min(1, "Channel ID can't be empty"),
+  userId: z.string().min(1, "User ID can't be empty"),
+  schedule: z.string().refine((value) => validate(value), {
+    message: 'Cron syntax error. Validate syntax at this <https://crontab.guru/|this site>.',
+  }),
+  message: z.string().trim().min(1, "Message can't be empty"),
+  ignoredMembers: z.array(z.string({ invalid_type_error: 'Invalid ID in ignored members' })),
+})
+
+export const validateSetupModalInput = (userInput: UserInputType) => {
+  const result = SetupModalValidationSchema.safeParse(userInput)
+  if (!result.success) {
+    return {
+      ...result,
+      error: result.error.flatten(),
+    }
+  }
+  return result
+}
+
+export const getModalData = (body: SlackViewAction): UserInputType => {
+  const values = get(body, ['view', 'state', 'values'])
+  const { channelId } = parseJson<{ channelId: string }>(get(body, ['view', 'private_metadata']))!
+  return {
+    channelId,
+    userId: get(body, ['user', 'id']),
+    schedule: get(values, [BlockIds.setup.cron, BlockIds.setup.cron, 'value'])!,
+    message: get(values, [BlockIds.setup.message, BlockIds.setup.message, 'value'])!,
+    ignoredMembers: get(
+      values,
+      [BlockIds.setup.ignoredMembers, BlockIds.setup.ignoredMembers, 'selected_users'],
+      []
+    ),
+  }
+}
+
+export const generateErrorMessage = (errors: typeToFlattenedError<UserInputType>) =>
+  reduce(
+    errors.fieldErrors,
+    (acc, error) => {
+      if (error) {
+        return [...acc, error.join('\n')]
+      }
+      return acc
+    },
+    [] as string[]
+  ).join('\n')
