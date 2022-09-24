8b8ae59e9bed2808143f9da57237726258ce3cc2
Jovan Blažek <36740941+jovanblazek@users.noreply.github.com>
2022-09-24 20:22:10 +0200

    Fix help command circular dependency

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,9 +1,11 @@
 import { Command } from '../classes'
 import { CommandNames } from '../constants'
-import { Commands } from './index'
+import ClearCommand from './clear'
+import ListCommand from './list'
+import PickCommand from './pick'
+import SetupCommand from './setup'
 
-// TODO improve this to remove circular dependency with commands/index.ts
-export default new Command(
+const HelpCommand = new Command(
   {
     name: CommandNames.help,
     description: 'Stop it! Get some help',
@@ -11,9 +13,19 @@ export default new Command(
   async ({ ack, respond }) => {
     await ack()
     await respond(
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
       Object.values(Commands)
         .map(({ params: { name, description } }) => `\`${name}\` ${description}`)
         .join('\n')
     )
   }
 )
+
+// Commands need to be exported from here due to circular dependency in HelpCommand
+export const Commands = {
+  ClearCommand,
+  ListCommand,
+  SetupCommand,
+  PickCommand,
+  HelpCommand,
+}
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,12 +1,1 @@
-import clear from './clear'
-import list from './list'
-import pick from './pick'
-import setup from './setup'
-
-// help command is left out to avoid circular dependency
-export const Commands = {
-  clear,
-  list,
-  setup,
-  pick,
-}
+export { Commands } from './help'
